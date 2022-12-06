d00e354f39ca05ee63f7fe297548f386ec92dc77
Zhu55kov <podmosk@mail.ru>
2022-12-06 14:33:22 +0300

    amended modificators JS popup and implemented most of valuable reviewer's notes

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,30 @@
-const page = document.querySelector('.page');
-const editButton = page.querySelector('.profile__edit-button');
-const popup = page.querySelector('.popup');
-const closeButton = page.querySelector('.popup__close');
-const profileTitle = page.querySelector('.profile__title');
-const profileSubtitle = page.querySelector('.profile__subtitle');
-const inputName = page.querySelector('.popup__input_form_name');
-const inputDescription = page.querySelector('.popup__input_form_description');
-const formSubmit = page.querySelector('.popup__form');
-
+const page = document.querySelector(".page");
+const editButton = page.querySelector(".profile__edit-button");
+const popup = page.querySelector(".popup");
+const closeButton = page.querySelector(".popup__close");
+const profileTitle = page.querySelector(".profile__title");
+const profileSubtitle = page.querySelector(".profile__subtitle");
+const form = page.querySelector(".popup__form");
+const inputName = form.querySelector(".popup__input_form_name");
+const inputDescription = form.querySelector(".popup__input_form_description");
 
 function editForm() {
-  popup.classList.add('popup_opened');
   inputName.value = profileTitle.textContent;
   inputDescription.value = profileSubtitle.textContent;
-};
-
+  popup.classList.add("popup_opened");
+}
 
 function closeForm() {
-  popup.classList.remove('popup_opened');
-  console.log(popup);
-};
+  popup.classList.remove("popup_opened");
+}
 
-
-function formSubmitHndlr(event) {
+function formSubmitHandler(event) {
   event.preventDefault();
   profileTitle.textContent = inputName.value;
   profileSubtitle.textContent = inputDescription.value;
   closeForm();
 }
-editButton.addEventListener('click', editForm);
-closeButton.addEventListener('click', closeForm);
-formSubmit.addEventListener('submit', formSubmitHndlr);
+
+editButton.addEventListener("click", editForm);
+closeButton.addEventListener("click", closeForm);
+form.addEventListener("submit", formSubmitHandler);
